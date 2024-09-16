aa1d1425aa77e598ae2da96e9e0f049f53c460d3
Emil Piękoś <piekosemil@gmail.com>
2024-09-16 16:40:18 +0200

    Liquibase implementation commit. Additional column with information about hire status.
    There is also additional file with sql. I was created it only to test if code working properly in production profile.

--- src/main/java/pl/javastart/bonuscalc/employee/EmployeeRepository.java
+++ src/main/java/pl/javastart/bonuscalc/employee/EmployeeRepository.java
@@ -1,8 +1,6 @@
 package pl.javastart.bonuscalc.employee;
 
-import jakarta.persistence.Table;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-@Table
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 }
